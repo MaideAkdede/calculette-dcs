5b28833304798598cb7c3b6ab19fb39fbec2057b
MaideAkdede <maide.akdede@student.hepl.com>
2021-02-04 16:59:16 +0100

    :tada: Initial commit.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+      Calculette
+    </title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <h1>Calculette</h1>
+  <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+      
+  </form>
+  </body>
+</html>
